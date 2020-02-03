1c39d1629970a29773058cc0c951848bb31cf799
renyuneyun (Rui ZHAO) <renyuneyun@gmail.com>
2020-02-03 23:48:45 +0000

    Better list items & add name

--- app/src/main/java/ryey/colorsniffer/MyViewHolder.kt
+++ app/src/main/java/ryey/colorsniffer/MyViewHolder.kt
@@ -2,14 +2,17 @@ package ryey.colorsniffer
 
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+    val textView_appName: TextView = itemView.findViewById(R.id.textView_appName)
     val imageView: ImageView = itemView.findViewById(R.id.imageView)
     val imageView2: ImageView = itemView.findViewById(R.id.imageView2)
 
     fun fillWith(appInfo: AppInfo, coloringMethod: ColoringMethod) {
+        textView_appName.text = appInfo.name
         imageView.setImageDrawable(appInfo.icon)
         imageView2.setImageDrawable(ColoringMethod.colorDrawable(appInfo, coloringMethod))
     }
