59b384f58d52ae8939f6a0006b8c5b8a01f78bab
renyuneyun (Rui ZHAO) <renyuneyun@gmail.com>
2020-02-18 21:47:11 +0000

    Refactor ColoringResult class and use `packageName/activityName` for id

--- app/src/main/java/ryey/colorsniffer/ColoringResult.kt
+++ app/src/main/java/ryey/colorsniffer/ColoringResult.kt
@@ -3,19 +3,19 @@ package ryey.colorsniffer
 import android.os.Bundle
 import ryey.colorsniffer.part.LauncherActivityInfo
 
-class ColoringResult(
+class ColoringResult private constructor(
     val defaultColor: Int,
     val coloringMethod: ColoringMethod,
-    val color: Map<String, Int>
+    private val color: Map<LauncherId, Int>
 ) {
 
     constructor(
         defaultColor: Int,
         coloringMethod: ColoringMethod,
         launcherActivityInfoList: List<LauncherActivityInfo>
-    ) : this(defaultColor, coloringMethod, HashMap<String, Int>().apply {
+    ) : this(defaultColor, coloringMethod, HashMap<LauncherId, Int>().apply {
         for (launcherActivityInfo in launcherActivityInfoList) {
-            this[launcherActivityInfo.packageName] =
+            this[LauncherId(launcherActivityInfo)] =
                 ColoringMethod.color(launcherActivityInfo, coloringMethod, defaultColor)
         }
     }
@@ -25,8 +25,8 @@ class ColoringResult(
         val extras = Bundle()
         extras.putBundle(IPC.KEY_COLOR_BUNDLE, Bundle().apply {
             putInt(IPC.KEY_DEFAULT_COLOR, defaultColor)
-            for ((appId, color) in color) {
-                putInt(appId, color)
+            for ((id, color) in color) {
+                putInt(id.toString(), color)
             }
         })
         return extras
@@ -40,11 +40,33 @@ class ColoringResult(
         return buf.toString()
     }
 
+    private data class LauncherId(val appId: String, val activityClass: String) {
+        constructor(launcherActivityInfo: LauncherActivityInfo) : this(
+            launcherActivityInfo.packageName,
+            launcherActivityInfo.klass
+        )
+
+        override fun toString(): String {
+            return "%s/%s".format(appId, activityClass)
+        }
+
+        companion object {
+            fun from(repr: String): LauncherId? {
+                val parts = repr.split("/")
+                if (parts.size != 2 || parts[0].isBlank() || parts[1].isBlank())
+                    return null
+                return LauncherId(parts[0], parts[1])
+            }
+        }
+    }
+
     class Partial(
         var defaultColor: Int? = null,
-        var coloringMethod: ColoringMethod? = null,
-        var color: Map<String, Int>? = null
+        var coloringMethod: ColoringMethod? = null
     ) {
+
+        private var color: Map<LauncherId, Int>? = null
+
         fun asColoringResult(): ColoringResult? {
             defaultColor?.let { defaultColor ->
                 coloringMethod?.let { coloringMethod ->
@@ -64,14 +86,18 @@ class ColoringResult(
                 } else {
                     null
                 }
-                val coloring: HashMap<String, Int> = HashMap()
+                val coloring: HashMap<LauncherId, Int> = HashMap()
                 extras.getBundle(IPC.KEY_COLOR_BUNDLE)?.let {
                     for (appId in it.keySet()) {
                         val color = it.getInt(appId)
-                        coloring[appId] = color
+                        LauncherId.from(appId)?.let { id ->
+                            coloring[id] = color
+                        }
                     }
                 }
-                return Partial(defaultColor, color = coloring)
+                return Partial(defaultColor).apply {
+                    color = coloring
+                }
             }
         }
     }
